c49057203701f233232204421fd40515e9c6bdd1
MohAlshahrani <90883554+MohAlshahrani@users.noreply.github.com>
2022-10-12 23:36:30 -0400

    native packages instead of PAPI

--- program0.c
+++ program0.c
@@ -1,50 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <papi.h>
+#include <time.h>
 
-//int main() {
-    /*
-    float real_time, proc_time, mflops;
-    long long flpops;
-    int retval;
+float program_zero(int NB){
+
+    uint64_t flops = 0 ;
 
     int i,j,k,m,K,n ;
-    n = m = K = 500;
-    double a[n][K],b[K][m],c[n][m];
+    n = m = K = NB;
+    float a[n][K],b[K][m],c[n][m];
     for (i = 0 ; i < n; i++) {
         for (j = 0; j < m; j++) {
             a[i][j] = rand();
             b[i][j] = rand();
         }
     }
-/*
-    if ( (retval = PAPI_flops_rate(PAPI_FP_OPS, &real_time, &proc_time, &flpops, &mflops)) < PAPI_OK )
-    {
-        printf("Could not initialise PAPI_flops \n");
-        printf("Your platform may not support floating point operation event.\n");
-        printf("retval: %d\n", retval);
-        exit(1);
-    }
-
+    time_t begin = clock();
     for(i=0; i<n; i++) {
         for (j = 0; j < m; j++) {
             for (k = 0; k < K; k++) {
                 c[i][j] += a[i][k] * b[k][j];
+                flops += 2;
             }
         }
     }
-/*
-    if((retval=PAPI_flops_rate(PAPI_FP_OPS,&real_time, &proc_time, &flpops, &mflops))<PAPI_OK)
-    {
-        printf("retval: %d\n", retval);
-        exit(1);
-    }
 
-    printf("Real_time: %f Proc_time: %f flpops: %lld MFLOPS: %f\n",
-           real_time, proc_time,flpops,mflops);
+    time_t end = clock();
 
-    exit(0);
-*/
-   // return 0;
+    double flops_s = flops/(end - begin);
+    printf("NB = %d \t FLOPS/s = %d \n", NB, flops_s);
+    return 0;
+}
 
-//}
+int main() {
+
+    program_zero(16);
+    program_zero(20);
+    program_zero(24);
+    program_zero(28);
+    program_zero(32);
+    program_zero(36);
+    program_zero(40);
+    program_zero(44);
+    program_zero(48);
+    program_zero(52);
+    program_zero(56);
+    program_zero(60);
+    program_zero(64);
+    program_zero(68);
+    program_zero(72);
+    program_zero(76);
+    program_zero(80);
+
+    return 0;
+
+}
